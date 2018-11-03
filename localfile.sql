62cf9de46feb6594a413eb16d816d58f79c2acc3
nick <nickkc.cahng@aitgroup.com.tw>
2018-11-04 00:48:27 +0800

    version 2

---
+++ localfile.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Alphabet( id int not null, name text not null, Alpha text not null );
+	INSERT INTO Alphabet values(1,'Alpha','A'); 
+	INSERT INTO Alphabet values(2,'Bravo','B'); 
+	INSERT INTO Alphabet values(3,'Charlie','C'); 
+	INSERT INTO Alphabet values(4,'Delta','D');
+	INSERT INTO Alphabet values(5,'Echo','E'); 
+	INSERT INTO Alphabet values(6,'Foxtrot','F'); 
